44dedc2658e2e8cf8f44c54e8dcf44f5678e5646
Lexi Borcherding <2391465@uhv.edu>
2025-10-05 12:26:51 -0500

    adding practice 2 problem

---
+++ RNA_transcription.py
@@ -0,0 +1,11 @@
+# practice problem 2
+
+#input DNA seuqence 
+dna = "GATGGAACTTGACTACGTAAATT"
+
+#then change all T to U 
+#Replace function 
+rna = dna.replace("T", "U")
+
+#print revised RNA
+print(rna)
